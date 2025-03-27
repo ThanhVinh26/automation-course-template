9e0433c47dc0f5e69b066b61ca49a3779d9fc703
YourGitHubUsername <YourEmail@example.com>
2025-03-27 21:03:19 +0700

    update URL

--- src/test/java/com/LoginTest.java
+++ src/test/java/com/LoginTest.java
@@ -11,7 +11,7 @@ public class LoginTest extends BasicTest {
     @Test()
     public void loginTest() throws Exception {
         // Launch website
-        String url = "https://automationtestpro.com/";
+        String url = "https://www.youtube.com/watch?v=0Acc0OfVyIc";
         driver.get(url);
         Assert.assertEquals(driver.getCurrentUrl(), url);
     }
