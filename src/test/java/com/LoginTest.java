0cd57929d362968616266f2584b6096f9b31b2e5
Auto <thanhvinh231198@gmail.com>
2025-03-30 20:54:57 +0700

    task16

--- src/test/java/com/LoginTest.java
+++ src/test/java/com/LoginTest.java
@@ -1,9 +1,12 @@
 package com;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
 import com.utils.BasicTest;
+import com.utils.Utils;
 
 public class LoginTest extends BasicTest {
 
@@ -11,9 +14,24 @@ public class LoginTest extends BasicTest {
     @Test()
     public void loginTest() throws Exception {
         // Launch website
-        String url = "https://www.youtube.com/watch?v=Y6AiKryrDyk";
+        String url = "https://bantheme.xyz/hathanhauto/tai-khoan/";
         driver.get(url);
         Assert.assertEquals(driver.getCurrentUrl(), url);
+        Utils.hardWait();
+         //email
+        WebElement email=driver.findElement(By.xpath("//input[@id='username']"));
+        email.sendKeys("thanhvinh231198@gmail.com");
+
+        //pass
+        Utils.hardWait();
+        WebElement pass=driver.findElement(By.xpath("//input[@name='password']"));
+        pass.sendKeys("Vinhkute98@");
+        //CLickbtn
+        Utils.hardWait(3);
+        WebElement click=driver.findElement(By.xpath("//button[@name='login']"));
+        click.click();
+        WebElement content=driver.findElement(By.xpath("//div[@class='woocommerce-MyAccount-content']"));
+        Assert.assertTrue(content.getText().contains("thanhvinh231198"));
     }
 
 }
